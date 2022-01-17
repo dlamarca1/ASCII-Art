14b720520a3c20ad1b2afdd753bcb9aef7b60ca4
dlamarca1 <86493548+dlamarca1@users.noreply.github.com>
2022-01-17 03:02:38 -0300

    AscII

---
+++ AscIIArt/src/main/java/AscArtIIReader.java
@@ -0,0 +1,59 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+public class AscArtIIReader {
+
+    static final char FIRST_CHAR = 'a';
+    static final char LAST_CHAR = 'z';
+    static final char QU_MARK = LAST_CHAR + 1;
+
+    private static FileReader getFileReader() throws FileNotFoundException {
+        return new FileReader("src\\main\\java\\ASCIIArt1.txt");//reads first input file when provided with its path
+    }
+
+    private static FileReader getFileReader1() throws FileNotFoundException {
+        return new FileReader("src\\main\\java\\ASCIIArt2.txt");//reads second input file when provided with its path
+    }
+
+    public static void printChar(int line, char character, String[] wholeAlphabet, int width) {
+        int begin = (character - FIRST_CHAR) * width;
+        int end = begin + width;
+        System.out.print(wholeAlphabet[line].substring(begin, end));
+    }
+
+    public static void main(String[] args) throws FileNotFoundException { //more files can be read by modifying x to the new amount of files being read in: Scanner[x]
+        Scanner[] sc = new Scanner[2];
+        sc[0] = new Scanner(getFileReader());
+        sc[1] = new Scanner(getFileReader1()); //if you want to read more files at the same time, please create an instance of sc[] for each file
+
+        for (int n = 0; n < 2; n++) { //where 2 is the number of input files, if more files are being read please change 2 to the current amount of files being read
+
+            //System.out.println("\n" + "ASCII Art from file: " + (n+1) + "\n"); //informs the user from which file the AscII Art is coming from
+
+            //width and height of one ASCII-art character
+            int width = sc[n].nextInt();
+            int height = sc[n].nextInt();
+
+            //text to convert
+            sc[n].nextLine();
+            String text = sc[n].nextLine().toLowerCase();
+
+            //whole alphabet + ? in ASCII Art
+            String[] rows = new String[height];
+            for (int i = 0; i < height; i++)
+                rows[i] = sc[n].nextLine();
+
+            //prints the result line by line and character
+            for (int l = 0; l < height; l++) {
+                for (char c : text.toCharArray())
+                    if (FIRST_CHAR <= c && c <= LAST_CHAR)
+                        printChar(l, c, rows, width);
+                    else
+                        printChar(l, QU_MARK, rows, width);
+                System.out.print("\n");
+            }
+        }
+    }
+}
